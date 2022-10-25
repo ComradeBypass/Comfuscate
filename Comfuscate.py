a0dc4ea59de53c01dd67eeccf38ab073cdc54a0b
Bypass <113047242+ComradeBypass@users.noreply.github.com>
2022-10-25 09:50:43 +0200

    Update Comfuscate.py

--- Comfuscate.py
+++ Comfuscate.py
@@ -403,26 +403,6 @@ def obfuscation(file):
     k = r"exec(codecs.decode(base64.b85decode(r'"+j+"')))"
 
     bruh = codecs.encode(k)
-    a = base64.b64encode(bruh)
-    b = codecs.decode(a)
-    c = r"exec(codecs.decode(base64.b64decode(r'"+b+"')))"
-
-    bruh = codecs.encode(c)
-    d = base64.b32encode(bruh)
-    e = codecs.decode(d)
-    f = r"exec(codecs.decode(base64.b32decode(r'"+e+"')))"
-
-    bruh = codecs.encode(f)
-    g = base64.b16encode(bruh)
-    h = codecs.decode(g)
-    ch = r"exec(codecs.decode(base64.b16decode(r'"+h+"')))"
-
-    bruh = codecs.encode(ch)
-    i = base64.b85encode(bruh)
-    j = codecs.decode(i)
-    k = r"exec(codecs.decode(base64.b85decode(r'"+j+"')))"
-
-    bruh = codecs.encode(k)
     a = str(zlib.compress(bruh, 9))
     c = r"exec(codecs.decode(zlib.decompress("+"bytes("+a+")"+")))"
 
